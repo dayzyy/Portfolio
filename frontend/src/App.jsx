43716c2e181277e2bee1013897a88c290357379a
Luka Mania <manialuka19@gmail.com>
2025-03-02 22:05:57 +0400

    feat(fronted) clean up the vite template code. set up some style varbiales. create the header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+import Logo from "./components/Logo.jsx"
+import CV from "./components/CV.jsx"
+
+export default function App() {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'original')
+
+  useEffect(_ => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+  }, [theme])
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <main className="bg-[var(--color-bg)]">
+      	<section className="h-screen w-screen p-4">
+	    <header className="flex justify-between items-center">
+		<h1 style={{fontSize: 'var(--size-text-heading)', fontFamily: 'var(--font-family-heading)'}} className="text-[var(--color-text-logo)]  font-bold"><Logo/></h1>
+		<CV/>
+	    </header>
+      	</section>
+      </main>
   )
 }
-
-export default App
---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react"
+
+export default function Logo() {
+    const [text, setText] = useState('<@dayzyy/>')
+
+    return text
+}
