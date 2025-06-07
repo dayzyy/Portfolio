6e235ce95440ace728593d3d4a88f1097f06302f
Luka Mania <manialuka19@gmail.com>
2025-06-07 20:50:50 +0400

    feat(frontend): Update the SkillCards and CV

--- frontend/src/components/Skills.jsx
+++ frontend/src/components/Skills.jsx
@@ -1,19 +1,17 @@
 import SkillCard from "./SkillCard";
-import { IoLogoJavascript } from "react-icons/io5";
+import { SiTypescript } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
-import { FaHtml5 } from "react-icons/fa6";
-import { FaCss3Alt } from "react-icons/fa6";
+import { SiKonva } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaPython } from "react-icons/fa6";
 import { TbBrandDjango } from "react-icons/tb";
 
 export default function Skills() {
 	const skills = [
-		{ language: "JavaScript", icon: <IoLogoJavascript /> },
-		{ language: "React", icon: <FaReact /> },
-		{ language: "HTML", icon: <FaHtml5 /> },
-		{ language: "CSS", icon: <FaCss3Alt /> },
-		{ language: "Tailwind", icon: <RiTailwindCssFill /> },
+		{ language: "TypeScript", icon: <SiTypescript /> },
+		{ language: "React.js", icon: <FaReact /> },
+		{ language: "Konva.js", icon: <SiKonva /> },
+		{ language: "TailwindCSS", icon: <RiTailwindCssFill /> },
 		{ language: "Python", icon: <FaPython /> },
 		{ language: "Django", icon: <TbBrandDjango /> },
 	];
