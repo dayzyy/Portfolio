d2259343c9f481dd6b69fcfbfbe6ce67c675df42
Luka Mania <manialuka19@gmail.com>
2025-06-21 02:54:36 +0400

    refactor(frontend): use `useResolution` windowWidth state instead of local EventListener

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import ImageGallery from "./ImageGallery.jsx";
 
 import { observe, unobserve } from "../utils/sectionObserver.js";
+import { useResolution } from "../contexts/ResolutionContext.jsx";
 
 import { FaAngleLeft } from "react-icons/fa";
 import { FaAngleRight } from "react-icons/fa";
@@ -10,10 +11,10 @@ import DropDowns from "./DropDowns.jsx";
 export default function Project({ project }) {
 	const [inFocus, setInFocus] = useState(0);
 	const [showGallery, setShowGallery] = useState(false);
-	const [vpMobile, setVpMobile] = useState(window.innerWidth <= 768);
 	const changeFocusTimer = useRef(null);
 	const sectionRef = useRef(null);
 	const [isVisible, setIsVisible] = useState(false)
+	const {windowWidth} = useResolution()
 
 	const reset_interval = () => {
 		clearInterval(changeFocusTimer.current)
@@ -58,18 +59,7 @@ export default function Project({ project }) {
 	}, [showGallery, isVisible]);
 
 	useEffect((_) => {
-			const handle_resize = (_) => {
-				setVpMobile(window.innerWidth < 768);
-			};
-
-			if (isVisible) {
-				window.addEventListener("resize", handle_resize);
-			} else {
-				window.removeEventListener("resize", handle_resize);
-				clearInterval(changeFocusTimer.current)
-			}
-
-			return (_) => window.removeEventListener("resize", handle_resize);
+		if (!isVisible) clearInterval(changeFocusTimer.current)
 	}, [isVisible]);
 
 	return (
@@ -79,26 +69,26 @@ export default function Project({ project }) {
 					images={project.screenshots}
 					focused_image={inFocus}
 					toggle_off={(_) => setShowGallery(false)}
-					vp_mobile={vpMobile}
+					vp_mobile={(windowWidth < 768)}
 				/>
 			)}
 
-			{vpMobile && <h1 className="large text-[var(--color-icon-lang)]">{project.name}</h1>}
+			{(windowWidth < 768) && <h1 className="large text-[var(--color-icon-lang)]">{project.name}</h1>}
 
-			<DropDowns vpMobile={vpMobile} project={project}/>
+			<DropDowns vpMobile={(windowWidth < 768)} project={project}/>
 
 			<div className="flex-grow flex flex-col gap-4 items-start mt-5">
-				{!vpMobile && <h1 className="large text-[var(--color-icon-lang)]">{project.name}</h1>}
+				{!(windowWidth < 768) && <h1 className="large text-[var(--color-icon-lang)]">{project.name}</h1>}
 
 				{!showGallery && // Display screenshots only when the gallery is toggled off
 					<>
 						<div className={`screenshots relative w-full overflow-x-hidden flex flex-col items-center self-center overflow-x-hidden
-							${(vpMobile && !project.suitableForMobile) ? 'h-fit' : 'h-0 flex-grow'}`}>
+							${((windowWidth < 768) && !project.suitableForMobile) ? 'h-fit' : 'h-0 flex-grow'}`}>
 							{project.screenshots.names.map((screen, index) => {
 								return (
 									<img
 										key={screen}
-										className={`${vpMobile ? 'max-h-full' : 'max-h-full'} rounded cursor-pointer
+										className={`${(windowWidth < 768) ? 'max-h-full' : 'max-h-full'} rounded cursor-pointer
 									${
 											inFocus == index
 												? "show-screen border border-rounded border-[var(--color-bg-card)]"
@@ -111,14 +101,14 @@ export default function Project({ project }) {
 												? { zIndex: index * 200 }
 												: { zIndex: index * 10 }
 										}
-										src={`screenshots/${project.screenshots.dir}/${project.suitableForMobile && vpMobile ? "mobile" : "desktop"}/${screen}`}
+										src={`screenshots/${project.screenshots.dir}/${project.suitableForMobile && (windowWidth < 768) ? "mobile" : "desktop"}/${screen}`}
 										onClick={(_) => handle_click_image(index)}
 									/>
 								);
 							})}
 						</div>
 
-						{vpMobile && (
+						{(windowWidth < 768) && (
 							<div className="min-h-fit flex flex-col items-center w-full">
 								<div className="flex w-5/6 justify-around">
 									<FaAngleLeft
@@ -140,7 +130,7 @@ export default function Project({ project }) {
 									/>
 								</div>
 
-								{(vpMobile && !project.suitableForMobile) && <p className="self-end">! this project is not suitable for mobile usage</p>}
+								{((windowWidth < 768) && !project.suitableForMobile) && <p className="self-end">! this project is not suitable for mobile usage</p>}
 
 								<a className="self-end" href={project.link}>{project.link}</a>
 							</div>
@@ -148,7 +138,7 @@ export default function Project({ project }) {
 					</>
 				}
 
-				{!vpMobile && <a className="self-end" href={project.link}>{project.link}</a>}
+				{!(windowWidth < 768) && <a className="self-end" href={project.link}>{project.link}</a>}
 			</div>
 
 		</section>
